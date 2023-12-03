412eb7f814e5c1604584fca0621db29d0fbe1285
Christian Gordon <cgordon6197@gmail.com>
2023-12-03 11:51:31 -0600

    Remove unneeded comments and clean up

--- Day3/GearRatios.py
+++ Day3/GearRatios.py
@@ -4,10 +4,6 @@ import math, re
 
 file = list(open('input.txt'))
 chars = {(index, value): [] for index in range(140) for value in range(140) if file[index][value] not in '0123456789.'}
-# print("First: %s", chars)
-# 	for c in range(140):
-# 		if file[r][c] not in '0123456789.':
-# 			chars = (r, c)
 
 for index, row in enumerate(file):
     for n in re.finditer(r'\d+', row):
@@ -16,15 +12,6 @@ for index, row in enumerate(file):
 
         for o in edge & chars.keys():
         	chars[o].append(int(n.group()))
-
-# for row, line in enumerate(file):
-# 	for n in re.finditer(r'\d+', line):
-# 		for row in (row - 1, row, row + 1):
-# 			for col in range(n.start() - 1, n.end() + 1):
-# 				edge = (row, col)
-
-		# for o in edge & chars.keys():
-		# 	chars[o].append(int(n.group()))
 
 part1 = sum(sum(part1) for part1 in chars.values()) 
 part2 = sum(math.prod(part2) for part2 in chars.values() if len(part2) == 2)
